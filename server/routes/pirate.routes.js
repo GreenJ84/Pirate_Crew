118b8baee8590910af6e1982be7dbba7e26e056e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-11 12:30:52 -0700

    change server routes for backend API

--- server/routes/pirate.routes.js
+++ server/routes/pirate.routes.js
@@ -2,10 +2,10 @@ const PirateController = require('../controllers/pirate.controller');
 
 module.exports = app => {
     
-    app.get('/pets/get', PirateController.findAll);
-    app.get('/pet/:id', PirateController.findOne);
-    app.post('/pet/new', PirateController.createNew);
-    app.put('/pet/update/:id', PirateController.update);
-    app.delete('/pet/delete/:id', PirateController.delete);
+    app.get('/pirates/get', PirateController.findAll);
+    app.get('/pirate/:id', PirateController.findOne);
+    app.post('/pirate/new', PirateController.createNew);
+    app.put('/pirate/update/:id', PirateController.update);
+    app.delete('/pirate/delete/:id', PirateController.delete);
     
 }
