d6bfd2f5a853444491807a16b29e5de0311eccfb
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-11 15:20:10 -0700

    updated style of pirate info display

--- client/src/views/Display.jsx
+++ client/src/views/Display.jsx
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import NavBar from '../components/NavBar';
 
+const getColor = () => {
+    const colors = [
+        'brown',
+        'grey',
+        'darkred',
+        'lightblue',
+        'lightgrey'
+    ]
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random]
+}
+const divStyle ={
+    backgroundColor: getColor(),
+    padding: '5%',
+    width: '60%',
+    margin: '0px 20% 4%',
+    borderRadius: '10px'
+}
+const button = {
+    color: 'white',
+    textDecoration: 'none',
+    backgroundColor: 'black',
+    padding: '2% 5%',
+    borderRadius: '3px',
+    boxShadow: '3px 3px black'
+}
 const Display = () => {
     const { id } = useParams();
     const [pirate, setPirate] = useState();
@@ -74,22 +101,24 @@ const Display = () => {
         <>
         {errors.map((err, index) => <p key={index}>{err}</p>)}
         { loaded && <>
+        <NavBar route='/' link='Crew Board'/>
         <div>
-                <h1> {pirate.pirate_name} </h1>
-                <Link to='/'> Home </Link>
+                <h1 style={{ borderBottom: '1px solid black', width: '50%', margin: '0px 25% 5%'}}> {pirate.pirate_name} </h1>
         </div>
         <img style={imgStyle} alt='' src={pirate.image_url}/>
-        <h2>{pirate.catch_phrase}</h2>
-        <div>
-            <h2>About</h2>
+        <h2>~ <em>{pirate.catch_phrase}</em></h2>
+        <div style={ divStyle }>
+            <h2 style={{ borderBottom: '1px solid black', width: '50%', margin: '0px 25% 5%'}}>About</h2>
             <p>Position: {pirate.position}</p>
             <p>Treasures: {pirate.num_of_chests}</p>
-            <p>Peg Leg: {pirate.attributes.pegLeg ? 'Yes' : 'No'} <button ></button></p>
+            <p>Peg Leg: {pirate.attributes.pegLeg ? 'Yes' : 'No'} </p>
             <p>Eye Patch: {pirate.attributes.eyePatch ? 'Yes' : 'No'}</p>
             <p>Hook Hand: {pirate.attributes.hookHand ? 'Yes' : 'No'} </p>
-
+            <br/>
+            <br/>
+            <Link style={ button } to={'/update/'+pirate._id}> Edit </Link>
         </div>
-        <Link to={'/update/'+pirate._id}> Edit </Link>
+        
         </>}</>
     )
 }
