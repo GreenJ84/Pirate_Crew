578065c15382d7f38b32f25e8d7afc58f6d4d8d0
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-11 15:17:53 -0700

    Added style to delete button

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -1,6 +1,20 @@
 import axios from "axios";
 import React from "react";
 
+const button ={
+    margin: '2% 4% 2%',
+    fontSize: 'smaller',
+    backgroundColor: 'red',
+    width: '55%',
+    height: '10%',
+    padding: '1%',
+    color:'black',
+    textDecoration: 'none',
+    border: '1px solid black',
+    borderRadius: '8px'
+}
+
+
 const DeleteButton = props => {
     const { pirateId, successCallback } = props;
 
@@ -11,7 +25,7 @@ const DeleteButton = props => {
     };
 
     return (
-        <button onClick={ deletePirate }>
+        <button style={ button } onClick={ deletePirate }>
             Walk the plank
         </button>
     )
